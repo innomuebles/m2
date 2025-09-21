02b88e008e100ffde8591ed5dd19dbfecee87e47
Dmitrii Fediuk <dfediuk@gmail.com>
2025-09-21 14:38:07 +0100

    https://github.com/innomuebles/m2/issues/45

--- vendor/magento/module-catalog/Model/View/Asset/Image.php
+++ vendor/magento/module-catalog/Model/View/Asset/Image.php
@@ -130,7 +130,17 @@ class Image implements LocalInterface
             case CatalogMediaConfig::IMAGE_OPTIMIZATION_PARAMETERS:
                 return $this->getUrlWithTransformationParameters();
             case CatalogMediaConfig::HASH:
-                return $this->context->getBaseUrl() . DIRECTORY_SEPARATOR . $this->getImageInfo();
+				# 2025-09-21 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+				# 1) "Adapt the website to Windows-based servers": https://github.com/innomuebles/m2/issues/45
+				# 2) "How to adapt `Magento\Catalog\Model\View\Asset\Image::getUrl()` to Windows
+				# in Magento ≥ 2.4.2?": https://mage2.pro/t/6411
+				# 3.1) "The `Swissup_Pagespeed` module breaks URLs of images in Windows":
+				# https://github.com/mydreamday-fi/site/issues/19
+				# 3.2) "The `Swissup_Pagespeed` module breaks URLs of products' images in Windows":
+				# https://github.com/mydreamday-fi/site/issues/18
+				# 3.3) "The `Swissup_Pagespeed` module breaks URLs of categories' images in Windows":
+				# https://github.com/mydreamday-fi/site/issues/17
+                return $this->context->getBaseUrl() . '/' . $this->getImageInfo();
             default:
                 throw new LocalizedException(
                     __("The specified Catalog media URL format '$this->mediaFormatUrl' is not supported.")
